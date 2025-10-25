d8d67a963270aa6a63ed04021e764e69a92ddb9d
sbrahma0 <sbrahma0@gmail.com>
2025-10-24 20:05:03 -0700

    Adding memeory to the RAG pipeline

--- groq_setup.py
+++ groq_setup.py
@@ -5,14 +5,14 @@ from langchain_groq import ChatGroq
 load_dotenv()
 
 
-def get_groq_model():
+def get_groq_model(model="llama-3.1-8b-instant"):
     """Initialize the Groq LLM."""
     api_key = os.getenv("GROQ_API_KEY")
     if not api_key:
         raise ValueError("GROQ_API_KEY not found in .env")
 
     llm = ChatGroq(
-        model="llama-3.1-8b-instant",  # or smaller if preferred
+        model=model,  # or smaller if preferred
         api_key=api_key,
         temperature=0,
     )
--- main.py
+++ main.py
@@ -2,19 +2,42 @@ from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from vector import retriever
 from groq_setup import get_groq_model
- 
+
+# for memory
+from langchain_core.runnables import RunnableWithMessageHistory
+from langchain_core.chat_history import InMemoryChatMessageHistory
 
 model = get_groq_model()
 
 template = """
 You are expert in answering questions about a pizza restaurant
 
-Here are some relevant reviews: {reviews}
+Here are some relevant reviews: 
+{reviews}
 
-Here is the question to answer: {question}
+Here is the question to answer: 
+{question}
 """
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
+
+# Wrapping the chain with memory to keep track of the conversation
+session_store = {}
+
+
+def get_session_history(session_id: str):
+    if session_id not in session_store:
+        session_store[session_id] = InMemoryChatMessageHistory()
+    
+    return session_store[session_id]
+
+
+runnable_with_memory = RunnableWithMessageHistory(
+    chain,
+    get_session_history,
+    input_messages_key="question",
+    history_messages_key="reviews"
+)
 
 while True:
     print("\n\n --------------------------------------------------")
@@ -23,7 +46,8 @@ while True:
     if question == "q":
         break
     reviews = retriever.invoke(question)
-    result = chain.invoke({"reviews": reviews, "question": question})
+    result = runnable_with_memory.invoke({"question": question, "reviews": reviews},
+                                         config={"configurable":{"session_id":"default"}})
     print(result.content if hasattr(result, "content") else result)
     print("-" * 80)
 
